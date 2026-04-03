d6f5e93d40ca5a9eed3d60900baa7177e5b02a25
Nikita Nesterov <nikita.nesterov@tiltshift.xyz>
2026-04-03 22:12:37 +0700

    [Dev][Game data] - create game data basics

---
+++ Assets/_Project/Code/Scripts/GameConfig.cs
@@ -0,0 +1,13 @@
+﻿using UnityEngine;
+
+namespace _Project.Code.Scripts
+{
+    [CreateAssetMenu(fileName = "GameData", menuName = "GameData")]
+    public class GameConfig: ScriptableObject
+    {
+        public int CreditStartAmount;
+        public int CrystalStartAmount;
+        public int PolymerStartAmount;
+        public int NanoGelStartAmount;
+    }
+}
